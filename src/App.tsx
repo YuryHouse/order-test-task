431e030e486b9d5fbfdfdd14ada4b6d83fb6b82c
YuryHouse <yury.house@yahoo.com>
2022-11-14 17:35:20 +0300

    heroku-backend has been connected

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
 
     const onSubmit = (data: IFormInput, e: any) => {
         e.preventDefault();
-        axios.post('http://localhost:3010/sendMessage', {data})
+        axios.post('https://gmail-nodejs-main.herokuapp.com/sendMessage', {data})
             .then((res) => {
                 alert('Thank you. Your message has been received. We will contact you shortly.');
             });
